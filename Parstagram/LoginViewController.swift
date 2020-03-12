6e497768351375d6f4259c8426329d349c92d6a3
Daniel Adu-Djan <adudjan.danieladd@gmail.com>
2020-03-11 21:09:50 -0400

    login page ui - image, label, text fields

---
+++ Parstagram/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  Parstagram
+//
+//  Created by Daniel Adu-Djan on 3/11/20.
+//  Copyright © 2020 Daniel Adu-Djan. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
